cadc1aecdbde2dcbc4ccc6bfe1387423f47e029f
Eugene Hlushak <hlushak.e@gmail.com>
2023-06-05 14:28:28 +0300

    edit basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./GlobalStyle.js";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/delivery-app-frontend">
+    <BrowserRouter basename="/">
       <App />
       <GlobalStyle />
     </BrowserRouter>
