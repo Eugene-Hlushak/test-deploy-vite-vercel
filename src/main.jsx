4e1635d589effba1ee5c699520cab91179f9d45a
Eugene Hlushak <hlushak.e@gmail.com>
2023-06-05 14:20:50 +0300

    add App.js and main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import { GlobalStyle } from "./GlobalStyle.js";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter basename="/delivery-app-frontend">
+      <App />
+      <GlobalStyle />
+    </BrowserRouter>
+  </React.StrictMode>
+);
